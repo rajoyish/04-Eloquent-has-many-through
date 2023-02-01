aba22c89e88df788f716fe515f7c203c722783d2
Himalayan <hdnpress@gmail.com>
2023-02-01 13:59:04 +0545

    List all projects with deployments

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Models\Project;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,13 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $projects = Project::with('deployments')
-        ->get();
-
-    return $projects;
-
     return view('welcome');
 });
+
+Route::resource('projects', ProjectController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
